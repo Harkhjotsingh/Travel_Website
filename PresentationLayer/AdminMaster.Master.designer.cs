742ee60ce4a58f2abb0e089028689f6f7a8e505c
harkhjotsingh <harkhjotsingh@gmail.com>
2017-02-15 17:12:10 -0500

    Started Styling of the Pages. Applied styling to MainMaster Page. Also using HTML5 for controls/tags rather than ASP controls/tags.

--- PresentationLayer/AdminMaster.Master.designer.cs
+++ PresentationLayer/AdminMaster.Master.designer.cs
@@ -40,15 +40,6 @@ namespace PresentationLayer {
         protected global::System.Web.UI.WebControls.Image LogoAdminMasterPage;
         
         /// <summary>
-        /// Menu1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
--- PresentationLayer/MainMaster.Master.designer.cs
+++ PresentationLayer/MainMaster.Master.designer.cs
@@ -22,13 +22,13 @@ namespace PresentationLayer {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// form1 control.
+        /// mainMasterForm control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm mainMasterForm;
         
         /// <summary>
         /// Logo control.
@@ -38,24 +38,6 @@ namespace PresentationLayer {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image Logo;
-        
-        /// <summary>
-        /// Menu1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
-        
-        /// <summary>
-        /// TreeView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TreeView TreeView1;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
