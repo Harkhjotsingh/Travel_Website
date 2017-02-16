30c5ec0782f5ef2bbb62ccccf79ffa04cae6cf71
harkhjotsingh <harkhjotsingh@gmail.com>
2017-02-16 13:29:49 -0500

    Added header, footer and fixed List on Admin Master page. Also added style sheet for it. Done some basic Styling.

--- PresentationLayer/AdminMaster.Master.designer.cs
+++ PresentationLayer/AdminMaster.Master.designer.cs
@@ -31,13 +31,13 @@ namespace PresentationLayer {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// LogoAdminMasterPage control.
+        /// Logo control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Image LogoAdminMasterPage;
+        protected global::System.Web.UI.WebControls.Image Logo;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
@@ -47,14 +47,5 @@ namespace PresentationLayer {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-        
-        /// <summary>
-        /// TxtCopyright control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TxtCopyright;
     }
 }
