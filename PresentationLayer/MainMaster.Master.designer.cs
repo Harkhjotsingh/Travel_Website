6dfdadabcd5d96d28140d6bcbec5f55363de4022
harkhjotsingh <harkhjotsingh@gmail.com>
2017-02-16 11:58:50 -0500

    Added footer on Main Master

--- PresentationLayer/MainMaster.Master.designer.cs
+++ PresentationLayer/MainMaster.Master.designer.cs
@@ -47,14 +47,5 @@ namespace PresentationLayer {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-        
-        /// <summary>
-        /// TxtCopyright control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TxtCopyright;
     }
 }
